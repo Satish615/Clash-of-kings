22bf5610b3b9ed79e7a20381ccbb82016f4ae80f
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2017-10-27 02:36:40 -0700

    creating board config and squares

--- Final Chess/public/js/client.js
+++ Final Chess/public/js/client.js
@@ -21,22 +21,7 @@ var Client = (function(window) {
   var forfeitPrompt       = null;
 
 
-  /**
-   * Initialize the UI
-   */
-  var init = function(config) {
-    gameID      = config.gameID;
-    playerColor = config.playerColor;
-    playerName  = config.playerName;
-
-    container   = $('#game');
-    messages    = $('#messages');
-    board       = $('#board');
-    squares     = board.find('.square');
-
-    gameOverMessage     = $('#game-over');
-    pawnPromotionPrompt = $('#pawn-promotion');
-    forfeitPrompt       = $('#forfeit-game');
+ 
 
     gameClasses = "white black red orange pawn rook knight bishop queen king not-moved empty selected " +
                   "valid-move valid-capture valid-en-passant-capture valid-castle last-move";
@@ -79,3 +64,5 @@ var Client = (function(window) {
         'a0','b0','c0','d0','e0','f0','g0','h0','i0','j0','k0','l0','m0','n0','o0','p0','q0','r0','s0','t0','u0','v0','w0','x0','y0','z0'
 
     ];
+    
+    
