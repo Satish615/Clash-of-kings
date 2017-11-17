2528346fb66715817eba1d7f4107978e184c50f1
Shiva Padma <shivapadma322@gmail.com>
2017-11-16 22:27:02 -0800

    refactor transform squares logic

--- Final Chess/lib/Game.js
+++ Final Chess/lib/Game.js
@@ -737,127 +737,131 @@ function getMovesForPlayer(board){
   return moves;
     
 };
+var num2alpha = function (n) {
+    switch (n) {
 
+        case 1:
+            return 'a';
+        case 2:
+            return 'b';
+        case 3:
+            return 'c';
+        case 4:
+            return 'd';
+        case 5:
+            return 'e';
+        case 6:
+            return 'f';
+        case 7:
+            return 'g';
+        case 8:
+            return 'h';
+        case 9:
+            return 'i';
+        case 10:
+            return 'j';
+        case 11:
+            return 'k';
+        case 12:
+            return 'l';
+        case 13:
+            return 'm';
+        case 14:
+            return 'n';
+        case 15:
+            return 'o';
+        case 16:
+            return 'p';
+        case 17:
+            return 'q';
+        case 18:
+            return 'r';
+        case 19:
+            return 's';
+        case 20:
+            return 't';
+        case 21:
+            return 'u';
+        case 22:
+            return 'v';
+        case 23:
+            return 'w';
+        case 24:
+            return 'x';
+        case 25:
+            return 'y';
+        case 26:
+            return 'z';
+        default:
+            return 'i'; // out of bounds
+    }
+};
+
+var alpha2num = function (a) {
+    switch (a) {
+        case 'a':
+            return 1;
+        case 'b':
+            return 2;
+        case 'c':
+            return 3;
+        case 'd':
+            return 4;
+        case 'e':
+            return 5;
+        case 'f':
+            return 6;
+        case 'g':
+            return 7;
+        case 'h':
+            return 8;
+        case 'i':
+            return 9;
+        case 'j':
+            return 10;
+        case 'k':
+            return 11;
+        case 'l':
+            return 12;
+        case 'm':
+            return 13;
+        case 'n':
+            return 14;
+        case 'o':
+            return 15;
+        case 'p':
+            return 16;
+        case 'q':
+            return 17;
+        case 'r':
+            return 18;
+        case 's':
+            return 19;
+        case 't':
+            return 20;
+        case 'u':
+            return 21;
+        case 'v':
+            return 22;
+        case 'w':
+            return 23;
+        case 'x':
+            return 24;
+        case 'y':
+            return 25;
+        case 'z':
+            return 26;
+        default :
+            return 26; // out of bounds
+    }
+};
+
+/**
+ * Apply an x and y offset to a starting square to get a destination square.
+ * Returns the destination square on success or false on failure.
+ */
 var transformSquare = function(square, transform) {
     if(square!==null) {
-        var alpha2num = function (a) {
-            switch (a) {
-                case 'a':
-                    return 1;
-                case 'b':
-                    return 2;
-                case 'c':
-                    return 3;
-                case 'd':
-                    return 4;
-                case 'e':
-                    return 5;
-                case 'f':
-                    return 6;
-                case 'g':
-                    return 7;
-                case 'h':
-                    return 8;
-                case 'i':
-                    return 9;
-                case 'j':
-                    return 10;
-                case 'k':
-                    return 11;
-                case 'l':
-                    return 12;
-                case 'm':
-                    return 13;
-                case 'n':
-                    return 14;
-                case 'o':
-                    return 15;
-                case 'p':
-                    return 16;
-                case 'q':
-                    return 17;
-                case 'r':
-                    return 18;
-                case 's':
-                    return 19;
-                case 't':
-                    return 20;
-                case 'u':
-                    return 21;
-                case 'v':
-                    return 22;
-                case 'w':
-                    return 23;
-                case 'x':
-                    return 24;
-                case 'y':
-                    return 25;
-                case 'z':
-                    return 26;
-                default :
-                    return 26; // out of bounds
-            }
-        };
-
-        var num2alpha = function (n) {
-            switch (n) {
-                case 1:
-                    return 'a';
-                case 2:
-                    return 'b';
-                case 3:
-                    return 'c';
-                case 4:
-                    return 'd';
-                case 5:
-                    return 'e';
-                case 6:
-                    return 'f';
-                case 7:
-                    return 'g';
-                case 8:
-                    return 'h';
-                case 9:
-                    return 'i';
-                case 10:
-                    return 'j';
-                case 11:
-                    return 'k';
-                case 12:
-                    return 'l';
-                case 13:
-                    return 'm';
-                case 14:
-                    return 'n';
-                case 15:
-                    return 'o';
-                case 16:
-                    return 'p';
-                case 17:
-                    return 'q';
-                case 18:
-                    return 'r';
-                case 19:
-                    return 's';
-                case 20:
-                    return 't';
-                case 21:
-                    return 'u';
-                case 22:
-                    return 'v';
-                case 23:
-                    return 'w';
-                case 24:
-                    return 'x';
-                case 25:
-                    return 'y';
-                case 26:
-                    return 'z';
-                default:
-                    return 'i'; // out of bounds
-            }
-        };
-
         // Parse square
         var file = square[0];
         var rank = parseInt(square[1], 10);
