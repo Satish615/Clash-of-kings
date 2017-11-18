1220db68765e52f5202062055a363e696e27109c
Akansha Mehta <akansha.mehta@sjsu.edu>
2017-11-17 21:49:43 -0800

    Update fastclick.js

--- Final Chess/public/js/fastclick.js
+++ Final Chess/public/js/fastclick.js
@@ -425,3 +425,27 @@ FastClick.prototype.touchHasMoved = function(event) {
 
 	return false;
 };
+
+/**
+ * Attempt to find the labelled control for the given label element.
+ *
+ * @param {EventTarget|HTMLLabelElement} labelElement
+ * @returns {Element|null}
+ */
+FastClick.prototype.findControl = function(labelElement) {
+	'use strict';
+
+	// Fast path for newer browsers supporting the HTML5 control attribute
+	if (labelElement.control !== undefined) {
+		return labelElement.control;
+	}
+
+	// All browsers under test that support touch events also support the HTML5 htmlFor attribute
+	if (labelElement.htmlFor) {
+		return document.getElementById(labelElement.htmlFor);
+	}
+
+	// If no for attribute exists, attempt to retrieve the first labellable descendant element
+	// the list of which is defined here: http://www.w3.org/TR/html5/forms.html#category-label
+	return labelElement.querySelector('button, input:not([type=hidden]), keygen, meter, output, progress, select, textarea');
+};
