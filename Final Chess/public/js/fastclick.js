2c046ef7ddc35b84ad2c651bc4eae00640fe94cc
satish Kumar Gollaprolu <satishgollaprolu@gmail.com>
2017-11-18 03:47:15 -0800

    modifying mouse events permitted

--- Final Chess/public/js/fastclick.js
+++ Final Chess/public/js/fastclick.js
@@ -550,6 +550,12 @@ FastClick.prototype.onTouchEnd = function(event) {
  *
  * @returns {void}
  */
+
+/**
+ * On touch cancel, stop tracking the click.
+ *
+ * @returns {void}
+ */
 FastClick.prototype.onTouchCancel = function() {
 	'use strict';
 	this.trackingClick = false;
@@ -580,4 +586,27 @@ FastClick.prototype.onMouse = function(event) {
 		return true;
 	}
 
-	
+	// Derive and check the target element to see whether the mouse event needs to be permitted;
+	// unless explicitly enabled, prevent non-touch click events from triggering actions,
+	// to prevent ghost/doubleclicks.
+	if (!this.needsClick(this.targetElement) || this.cancelNextClick) {
+
+		// Prevent any user-added listeners declared on FastClick element from being fired.
+		if (event.stopImmediatePropagation) {
+			event.stopImmediatePropagation();
+		} else {
+
+			// Part of the hack for browsers that don't support Event#stopImmediatePropagation (e.g. Android 2)
+			event.propagationStopped = true;
+		}
+
+		// Cancel the event
+		event.stopPropagation();
+		event.preventDefault();
+
+		return false;
+	}
+
+	// If the mouse event is permitted, return true for the action to go through.
+	return true;
+};
