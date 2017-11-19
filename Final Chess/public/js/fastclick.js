3d51eae3e69ba8597237d7c372f9d1db3d1c8ab9
Akansha Mehta <akansha.mehta@sjsu.edu>
2017-11-18 19:50:33 -0800

    Update fastclick.js

--- Final Chess/public/js/fastclick.js
+++ Final Chess/public/js/fastclick.js
@@ -701,3 +701,77 @@ FastClick.notNeeded = function(layer) {
 
 	return false;
 };
+
+/**
+ * Factory method for creating a FastClick object
+ *
+ * @param {Element} layer The layer to listen on
+ */
+FastClick.attach = function(layer) {
+	'use strict';
+	return new FastClick(layer);
+};
+
+
+if (typeof define !== 'undefined' && define.amd) {
+
+	// AMD. Register as an anonymous module.
+	define(function() {
+		'use strict';
+		return FastClick;
+	});
+} else if (typeof module !== 'undefined' && module.exports) {
+	module.exports = FastClick.attach;
+	module.exports.FastClick = FastClick;
+} else {
+	window.FastClick = FastClick;
+}
+
+//function to define timestamps and update chatbox
+var me = {};
+
+var you = {};
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time = 0){
+    var control = "";
+    var date = formatAMPM(new Date());
+
+    if (who == "me"){
+
+        control = '<li style="width:100%">' +
+            '<div class="msj macro">' +
+            '<div class="text text-l">' +
+            '<p>'+ text +'</p>' +
+            '<p><small>'+date+'</small></p>' +
+            '</div>' +
+            '</div>' +
+            '</li>';
+    }else{
+        control = '<li style="width:100%;">' +
+            '<div class="msj-rta macro">' +
+            '<div class="text text-r">' +
+            '<p>'+text+'</p>' +
+            '<p><small>'+date+'</small></p>' +
+            '</div>' +
+            '<div class="avatar" style="padding:0px 0px 0px 10px !important"></div>' +
+            '</li>';
+    }
+    setTimeout(
+        function(){
+            $("ul").append(control);
+
+        }, time);
+
+}
